2d325ab82dd87815e4f721d70b90107ee4a19d42
Zhumgalbek <jumgalbek2002@icloud.com>
2023-01-03 16:58:31 +0300

    Merge pull request #9 from Eldiar004/jumgalbek
    commit method methodjava7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import entity.Person;
 
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,6 +18,23 @@ public class Main {
         Person person7 = new Person("Nuraiym",9);
         Person person8 = new Person("Eldiyar", 120);
         Person person9 = new Person("Argen" , 1244);
+
+        List<Person> people1 = Arrays.asList(person1, person2, person3, person4, person5, person6, person7, person8, person9);
+        List<Person> people = new ArrayList<>();
+
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+        people.add(person4);
+        people.add(person5);
+        people.add(person6);
+        people.add(person7);
+        people.add(person8);
+        people.add(person9);
+
+        for (Person person: people ) {
+            System.out.println(person);
+        }
 
 
     }

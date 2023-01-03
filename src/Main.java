c92534a9c2bb7a008d7befefde72ce47bf88f78f
User <mnurajym9@gmail.com>
2023-01-03 16:52:02 +0600

    add person list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import entity.Person;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,6 +16,7 @@ public class Main {
         Person person8 = new Person("Eldiyar", 120);
         Person person9 = new Person("Argen" , 1244);
 
+        List<Person> people = Arrays.asList(person1, person2, person3, person4, person5, person6, person7, person8, person9);
 
     }
 }
